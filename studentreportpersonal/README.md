ad2640fc938020fe65a91382e5808d6ec3257a8f
chrizteroo <108843788+chrizteroo@users.noreply.github.com>
2023-12-11 16:32:39 -0500

    reactReportCard

---
+++ studentreportpersonal/README.md
@@ -0,0 +1,23 @@
+1. Open Terminal create a folder
+
+2. npx create-react-app studentreportpersornal
+
+3. cd my stuudentreportpersonal
+
+4. open src folder
+
+5. create a folder called components
+
+6. inside the components folder create another folder called MySvgComponent.jsx
+STOP
+7. Modify App.js to include variable values of the svg
+
+8. Navigate to Public folder
+
+9. create folder called assets
+
+10. Import svg into assets folder
+
+9. run npm start
+
+ 
---
+++ studentreportpersonal/src/App.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+const App = () => {
+  const [textValues, setTextValues] = useState({
+    textxto: 'Christopher Oladimeji',
+    textTwenty: '2022/23',
+    textTen: 'Ten',
+    textSec: 'Second',
+    textGradeLa: 'O',
+    textGradeRe2: 'S',
+    textGradePre3: 'FS',
+    textGradeGeo4: 'NSH',
+    textGrade5Earth: 'S',
+    textSocial: 'O',
+    textArtGrade7: 'S',
+    textGrade2Puntual: 'O',
+    textGrade2NeatO: 'S',
+    textGrade2Polite: 'O',
+    // ... other default values for text elements ...
+  });
+
+  const handleTextChange = (id, value) => {
+    setTextValues(prev => ({ ...prev, [id]: value }));
+  };
+
+  const updateSVGText = useCallback(() => {
+    const svgObject = document.getElementById('reportCardSvg');
+    if (svgObject && svgObject.contentDocument) {
+      const svgDocument = svgObject.contentDocument;
+
+      Object.entries(textValues).forEach(([id, value]) => {
+        const textElement = svgDocument.getElementById(id);
+        if (textElement) textElement.textContent = value;
+      });
+    }
+  }, [textValues]);
+
+  useEffect(() => {
+    updateSVGText();
+  }, [updateSVGText]); // Dependency is now the useCallback reference
+
+  const resetToDefault = () => {
+    setTextValues({
+      textxto: 'Christopher Oladimeji',
+      textTwenty: '2022/23',
+      textTen: 'Ten',
+      textSec: 'Second',
+      textGradeLa: 'O',
+      textGradeRe2: 'S',
+      textGradePre3: 'FS',
+      textGradeGeo4: 'NSH',
+      textGrade5Earth: 'S',
+      textSocial: 'O',
+      textArtGrade7: 'S',
+      textGrade2Puntual: 'O',
+      textGrade2NeatO: 'S',
+      textGrade2Polite: 'O',
+      // ... other default values for text elements ...
+    });
+  };
+
+  return (
+    <div>
+      <object id="reportCardSvg" type="image/svg+xml" data="/ReportCard.svg" width="210mm" height="297mm">
+        Your browser does not support SVG
+      </object>
+
+      {/* Form for updating text */}
+      <form>
+        {Object.entries(textValues).map(([id, value]) => (
+          <label key={id}>
+            {id}:
+            <input type="text" value={value} onChange={(e) => handleTextChange(id, e.target.value)} />
+          </label>
+        ))}
+      </form>
+
+      <button onClick={updateSVGText}>Update Text</button>
+      <button onClick={resetToDefault}>Reset to Default</button>
+    </div>
+  );
+};
+
+export default App;
